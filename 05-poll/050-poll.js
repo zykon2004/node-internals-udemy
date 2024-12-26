dce321cd883ff207cd0ef7a091e30ecfb14724d1
Hussein <hnasser@example.com>
2024-12-26 23:42:10 +0000

    updated read file

--- 05-poll/050-poll.js
+++ 05-poll/050-poll.js
@@ -6,12 +6,12 @@ console.log(`START`);
 const readFileCallback = (err, data) => {
   console.log(`readFileCallback ${data}`);
 };
-const f = '/Users/HusseinNasser/projects/node-course-content/03-poll/test.txt'
+const f = 'test.txt'
 //in the intial phase this file will only be opened (not read)
 //if it fails we immediately know, we add a callback in the poll queue with failure.
 fs.readFile(f, readFileCallback);
 
 //slow down the initial phase
-for (let i = 1; i <= 100000000000; i++);
+for (let i = 1; i <= 10000000000; i++);
 //after the loop ends and initial phase is done, we enter the poll phase and actually read the file (no callbacks ready)
 console.log(`END`);
