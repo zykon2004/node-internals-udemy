5b79a5b72cab0085aed205186bb3233faff69ba6
Hussein <hnasser@example.com>
2025-01-21 03:09:31 +0000

    pusg

--- 22-child-process/224-cluster.js
+++ 22-child-process/224-cluster.js
@@ -19,10 +19,11 @@ if (cluster.isPrimary) {
 } else {
   // Workers can share any TCP connection
   // In this case it is an HTTP server
-  http.createServer((req, res) => {
+  const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end('hello world\n');
-  }).listen(8000);
+  }).listen(8000)
+  server.on("listening", ()=> console.log( process.pid + " listening.." + JSON.stringify(server))) 
 
   console.log(`Worker ${process.pid} started`);
 }
