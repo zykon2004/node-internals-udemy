ca582c8a735a78a4fd07bdecaf4a23b1380f1d51
Hussein Nasser <HusseinNasser@MacBook-Pro.local>
2025-02-02 10:16:06 -0800

    update numbering

---
+++ 26-request-showdown/272-undici-http.js
@@ -0,0 +1,49 @@
+const undici = require('undici');
+
+const myAgent = new undici.Agent({
+     "connections": 10 // set max connections
+});
+
+
+const requestCount = 100;
+const stats = {"undici":  {"reqtime": 0, "reqmem": 0, "restime": 0, "reqmem": 0}}
+const startRequest = Date.now()
+//sending multiple requests
+console.time("http.request")
+console.time( "http.response")
+ 
+const startRSS = process.memoryUsage().rss;
+
+//we can parse the URL once
+const url = new URL("http://example.org")
+const requests = []
+for (let i =0; i < requestCount; i++) {
+     //pass the parsed url
+     //if you pass the string , we parse it everytime
+    requests.push(sendRequest(url, {"dispatcher": myAgent, "method": "GET"} ))
+}
+ 
+console.log("submitted all requests");
+console.timeEnd ("http.request")
+let endRSSAllRequests = process.memoryUsage().rss;
+let endRequest = Date.now() 
+stats[Object.keys(stats)[0]].reqmem = (endRSSAllRequests - startRSS).toLocaleString()
+stats[Object.keys(stats)[0]].reqtime = endRequest - startRequest 
+console.log (`Memory used after sending all requests ${endRequest - startRequest} bytes`)
+let endRSSAllResponses;
+let endResponse;
+Promise.all(requests).then ( a=> {
+    console.timeEnd( "http.response")
+    endRSSAllResponses= process.memoryUsage().rss;
+    endResponse = Date.now();
+    stats[Object.keys(stats)[0]].resmem = (endRSSAllResponses - startRSS).toLocaleString()
+    stats[Object.keys(stats)[0]].restime = endResponse - startRequest;
+    console.log (`Memory used after receiving all responses ${(endRSSAllResponses - startRSS).toLocaleString()} bytes`)
+    console.table(stats)
+})
+//take a peek at pending
+//take a peek at pending
+ 
+function sendRequest(url, opt) {
+   return undici.request(url, opt);
+}
