07e0b96f390f6361cf68727fef512bd042a078f0
Hussein Nasser <hus.mhd@gmail.com>
2025-02-22 09:33:17 -0800

    new udp

--- 19-udp/191-UDP-Client.js
+++ 19-udp/191-UDP-Client.js
@@ -1,20 +1,22 @@
-const dgram = require('dgram')
+const dgram = require('dgram');
+const { isTypedArray } = require('util/types');
 const client = dgram.createSocket('udp4');
 
 client.on('message', (msg, rinfo) => {
   console.log(`client got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
+//this automatically binds to a random port, (can't send anything otherwise)
+client.send("hello server", 3333, "127.0.0.1")
 
-//optional but we can bind to a random port of our choice
-client.bind(55555);
+//optionally bind to a source port of your choosing
+//if you don't do that, kernel will pick one for you
+//client.bind(55555) 
 
-client.connect( 3333,"127.0.0.1", () => {
-  console.log ("server exists .. send something.. ")
-  //notice that its called send and not write 
-  //because it is not a stream of bytes ..but an actual unit (message) we send 
-  client.send("a whole message");
-});
-
+//technically if you know the clients bind source port anyone can send stuff to it ! 
+//try it
+////echo "Hello UDP" | nc -u 127.0.0.1 randomport
+client.on("error", (err) => console.log(err) )
+//a bind was successful! 
 client.on('listening', () => {
   const address = client.address();
   //this will keep reading
