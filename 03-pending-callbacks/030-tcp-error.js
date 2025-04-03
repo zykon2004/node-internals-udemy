319c5b5507e641facf10b5bbe343c83a44dc5e66
Monkey <Monkey@Tree.com>
2025-04-03 22:43:57 +0300

    added timeout, explored callbacks

--- 03-pending-callbacks/030-tcp-error.js
+++ 03-pending-callbacks/030-tcp-error.js
@@ -1,57 +1,43 @@
-//example where pending callbacks are scheduled in a different phase. 
-const net = require('net');
-const fs = require(`fs`);
- 
+// noinspection StatementWithEmptyBodyJS
+
+const net = require("net");
+const start = Date.now();
 console.log(`START`);
-// Define the IP and port
-//change the host to a none existant port host, 
-//the ECONNREFUSED will be queued in the pending callbacks phase output will be 
-/*START
-END
-time out
-Connection error: connect ECONNREFUSED 127.0.0.1:80
-Connection closed
-readFileCallback hello world
-*/
-//but if you change it to a valid ip/port say google's 
-//the output will be 
-/*
-START
-END
-time out
-readFileCallback hello world
-Connected to server at 142.250.188.23:80
-*/
 
- 
-
-// Create a client
 const clientFail = new net.Socket();
+clientFail.setTimeout(3000, () => timeoutHandler());
+clientFail.on("error", (err) => errorHandler(err, "clientFail"));
+// Calling it this way won't work because timeoutHandler is not yet defined
+// clientFail.setTimeout(3000, timeoutHandler);
+
+// It's also possible to do it this way:
+// clientFail.setTimeout(3000);
+// clientFail.on("timeout", timeoutHandler);
+const timeoutHandler = () => {
+  console.log(`Timeout after ${Date.now() - start}`);
+  // calling clientFail.end() will not end the program because the socket attempts to exit gracefully
+  clientFail.destroy(new Error("Trigger error callback for pleasure"));
+};
+
 const clientSuccess = new net.Socket();
+clientSuccess.on("error", (err) => errorHandler(err, "clientSuccess"));
 
-// Connect to the server (fails)
-clientFail.connect(9999,'192.168.4.21', () => {
-    console.log(`Connected to server at ${'192.168.4.21'}:${9999}`);
+const errorHandler = (err, clientName) => {
+  console.error(`Connection error on ${clientName}: ${err.message}`);
+  console.log(`Exited after ${Date.now() - start}`);
+};
+// Will be registered in the pending callback
+// That's why timeout is added, to end it early
+clientFail.connect(9999, "192.168.2.99", () => {
+  console.log(`Connected to server at 192.168.2.99:${9999}`);
 });
-// Connect to the server that exists (this is example.com)
-clientSuccess.connect(80, '93.184.215.14', () => {
-    console.log(`Connected to server at ${'93.184.215.14'}:${80}`);
-});
-
-// Handle errors
-clientFail.on('error', (err) => {
-    console.error(`Connection error: ${err.message}`);
-});
-// Handle errors
-clientSuccess.on('error', (err) => {
-    console.error(`Connection error: ${err.message}`);
+// Will be registered in the poll callback
+clientSuccess.connect(80, "142.250.188.23", () => {
+  console.log(`Connected to server at 142.250.188.23:${80}`);
+  clientSuccess.end();
+  console.log("Ended 142.250.188.23:80");
 });
 
-
- 
-//slow down the initial phase
-for (let i = 1; i <= 500000000; i++);
-//after the loop ends and initial phase is done, we enter the poll phase and actually read the file (no callbacks ready)
-setTimeout(()=>console.log("timer!"), 0)
+for (let i = 1; i <= 500_000_000; i++);
+setTimeout(() => console.log("timer!"), 0);
 console.log(`END`);
-
